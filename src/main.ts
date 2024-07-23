3c8a88e10e56cb6f3143e1b2d9f78f36cc715e95
Yoendi <yoendigy1408@gmail.com>
2024-07-23 11:42:31 -0400

    Firt commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
+import Login from "./pages/Login.vue";
 
-createApp(App).mount("#app");
+
+const routes = [
+    {
+        path: "/",
+        name: "Login",
+        component: Login
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  createApp(App).use(router).mount("#app");
